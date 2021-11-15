0eecf1ad1f440648d298faf39cc4b6f695150561
Jorge Ernesto Gomez Giraldo <ernesto2066@gmail.com>
2021-11-15 11:09:16 -0500

    Jorge Gomez - Configure path libraries and update instructions

--- test/system/database/ConexionIT.java
+++ test/system/database/ConexionIT.java
@@ -7,6 +7,8 @@ package system.database;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -43,14 +45,22 @@ public class ConexionIT {
      * Test of getConnection method, of class Conexion.
      */
     @Test
-    public void testGetConnection() {
+    public void testGetConnection() throws SQLException {
         System.out.println("getConnection");
+        String sql = "SELECT * FROM expenses";
         Conexion instance = new Conexion();
-        Connection expResult = null;
-        Connection result = instance.getConnection();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Connection expResult;
+        expResult = instance.getConnection();
+        // Recibe el resultado de la consulta SQL
+        try (Statement stmt = expResult.createStatement(); 
+                ResultSet rs = stmt.executeQuery(sql)) {
+            while(rs.next()) {
+                System.out.println ("Consulta de prueba : "+rs); 
+            }
+            // Cierra la conexión a la BD
+            rs.close();
+            stmt.close();
+        }
     }
 
     /**
